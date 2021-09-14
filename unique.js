d499a9d6d9f0f2b699ff109c939a0bf4c5f4e051
abhilashsthakur <abhilashsinghthakur@gmail.com>
2021-09-14 23:24:47 +0530

    getting the values does not match in both the arrays

---
+++ unique.js
@@ -0,0 +1,19 @@
+// var a=[1,2,3,4,5]
+
+// var b=[2,3,4,5,6]
+
+const a = [1,2,3,4,5]
+const b = [2,3,4,5,6]
+
+const uniqe = (first, second) => {
+
+   const myConcat=a.concat(b)
+   
+   return myConcat.filter(el => { return
+   
+      return !(a.includes(el) && b.includes(el));
+   
+   })
+
+};
+console.log(uniqe([1,2,3,4,5], [2,3,4,5,6]));
